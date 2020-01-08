2b229de5ee4efbdcee9870063ed5180a349ccaa9
Conor Samuel McFeely <cmcfeely03@qub.ac.uk>
2020-01-08 16:01:23 +0000

    fixed db connection thing and added json endpoint

--- gke/advert/app.py
+++ gke/advert/app.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import mysql.connector as mysql
 import os
+import json
 app = Flask(__name__)
 
 advert_ip = str(os.environ.get("ADVERTIP"))
-
-db = mysql.connect(
-    host = advert_ip,
-    user = "root",
-    passwd = "QUBccProject"
-)
 
 database_name = "cloudcomputingadverts.advert"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
+
+        db = mysql.connect(
+            host = advert_ip,
+            user = "root",
+            passwd = "QUBccProject"
+        )
+
+
         error = False
 
         advert_content = request.form["advertContent"]
@@ -42,8 +45,108 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/advertid')
+def getadvertid():
+
+    db = mysql.connect(
+        host = advert_ip,
+        user = "root",
+        passwd = "QUBccProject"
+    )
+
+    query_string = request.args.get("id")
+    print(query_string)
+
+    if(str(query_string).isdigit()):
+        cursor = db.cursor()
+        sql_query = "SELECT * FROM " + database_name + " WHERE advertid=" + str(query_string)
+        cursor.execute(sql_query)
+        advert = cursor.fetchall()
+        print(advert)
+
+        if(advert):
+            advert = advert[0]
+
+            advert_id = advert[0]
+            advert_content = advert[2]
+            advert_keyword = advert[1]
+            advert_url = advert[3]
+
+            dictionary_advert = {
+                "id":advert_id,
+                "content":advert_content,
+                "keywords":advert_keyword,
+                "url":advert_url
+            }
+
+            json_object = json.dumps(dictionary_advert)
+            resp = make_response(json_object)
+            resp.headers['Access-Control-Allow-Origin'] = '*'
+            resp.headers['Content-Type'] = 'application/json'
+            return(resp)
+        else:
+            return "no such advert"
+        
+    return "invalid id"
+
+
+@app.route('/advertkeyword')
+def getadvertkeyword():
+
+    db = mysql.connect(
+        host = advert_ip,
+        user = "root",
+        passwd = "QUBccProject"
+    )
+
+    query_string = request.args.get("keyword")
+    print(query_string)
+
+    if(query_string):
+        cursor = db.cursor()
+        sql_query = "SELECT * FROM " + database_name + " WHERE keyword=" +  "'" + str(query_string) + "'"
+        print()
+        cursor.execute(sql_query)
+        advert = cursor.fetchall()
+        print(advert)
+
+        if(advert):
+            advert = advert[0]
+
+            advert_id = advert[0]
+            advert_content = advert[2]
+            advert_keyword = advert[1]
+            advert_url = advert[3]
+
+            dictionary_advert = {
+                "id":advert_id,
+                "content":advert_content,
+                "keywords":advert_keyword,
+                "url":advert_url
+            }
+
+            json_object = json.dumps(dictionary_advert)
+            resp = make_response(json_object)
+            resp.headers['Access-Control-Allow-Origin'] = '*'
+            resp.headers['Content-Type'] = 'application/json'
+            return(resp)
+        else:
+            return "no such advert"
+        
+    return "invalid id"
+
+
+
 @app.route('/test')
 def test():
+
+    db = mysql.connect(
+        host = advert_ip,
+        user = "root",
+        passwd = "QUBccProject"
+    )
+
+
     cursor = db.cursor()
     sql = "SELECT * FROM " + database_name +";"
     cursor.execute(sql)
