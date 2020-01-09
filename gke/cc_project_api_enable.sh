e71fcd05a68e4d32329500804142859a890fcc00
Conor Samuel McFeely <cmcfeely03@qub.ac.uk>
2020-01-09 18:31:05 +0000

    rearrange and add new

--- gke/cc_project_api_enable.sh
+++ gke/cc_project_api_enable.sh
@@ -1,4 +1,6 @@
+#compute is a prerequsit for some so must come first
+gcloud services enable compute.googleapis.com
 gcloud services enable servicenetworking.googleapis.com 
 gcloud services enable container.googleapis.com
-gcloud services enable compute.googleapis.com
 gcloud services enable sqladmin.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
