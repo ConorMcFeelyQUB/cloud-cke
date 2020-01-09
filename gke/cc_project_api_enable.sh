cb482b62192305ed1f2d49a9a75bb643fb61a2d3
Conor Samuel McFeely <cmcfeely03@qub.ac.uk>
2020-01-09 17:12:02 +0000

    added script to enable apis

---
+++ gke/cc_project_api_enable.sh
@@ -0,0 +1,4 @@
+gcloud services enable servicenetworking.googleapis.com 
+gcloud services enable container.googleapis.com
+gcloud services enable compute.googleapis.com
+gcloud services enable sqladmin.googleapis.com
